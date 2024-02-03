f01d34e6bfa8f2a081141b0e616ce0cc237349f4
MalitsPlus <50281195+MalitsPlus@users.noreply.github.com>
2024-02-03 18:14:10 +0900

    refactor: convert indentation to spaces since these files themselves are also testdata

--- gofmt_test.go
+++ gofmt_test.go
@@ -5,15 +5,15 @@
 package main
 
 import (
-	"bytes"
-	"flag"
-	"os"
-	"path/filepath"
-	"strings"
-	"testing"
-	"text/scanner"
+  "bytes"
+  "flag"
+  "os"
+  "path/filepath"
+  "strings"
+  "testing"
+  "text/scanner"
 
-	"malitsplus/gofmtv/std/diff"
+  "malitsplus/gofmtv/std/diff"
 )
 
 var update = flag.Bool("update", false, "update .golden files")
@@ -26,105 +26,105 @@ var update = flag.Bool("update", false, "update .golden files")
 // and returns the flags string, if any. Otherwise it
 // returns the empty string.
 func gofmtFlags(filename string, maxLines int) string {
-	f, err := os.Open(filename)
-	if err != nil {
-		return "" // ignore errors - they will be found later
-	}
-	defer f.Close()
+  f, err := os.Open(filename)
+  if err != nil {
+    return "" // ignore errors - they will be found later
+  }
+  defer f.Close()
 
-	// initialize scanner
-	var s scanner.Scanner
-	s.Init(f)
-	s.Error = func(*scanner.Scanner, string) {}       // ignore errors
-	s.Mode = scanner.GoTokens &^ scanner.SkipComments // want comments
+  // initialize scanner
+  var s scanner.Scanner
+  s.Init(f)
+  s.Error = func(*scanner.Scanner, string) {}       // ignore errors
+  s.Mode = scanner.GoTokens &^ scanner.SkipComments // want comments
 
-	// look for //gofmt comment
-	for s.Line <= maxLines {
-		switch s.Scan() {
-		case scanner.Comment:
-			const prefix = "//gofmt "
-			if t := s.TokenText(); strings.HasPrefix(t, prefix) {
-				return strings.TrimSpace(t[len(prefix):])
-			}
-		case scanner.EOF:
-			return ""
-		}
-	}
+  // look for //gofmt comment
+  for s.Line <= maxLines {
+    switch s.Scan() {
+    case scanner.Comment:
+      const prefix = "//gofmt "
+      if t := s.TokenText(); strings.HasPrefix(t, prefix) {
+        return strings.TrimSpace(t[len(prefix):])
+      }
+    case scanner.EOF:
+      return ""
+    }
+  }
 
-	return ""
+  return ""
 }
 
 func runTest(t *testing.T, in, out string) {
-	// process flags
-	*simplifyAST = false
-	*rewriteRule = ""
-	info, err := os.Lstat(in)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	for _, flag := range strings.Split(gofmtFlags(in, 20), " ") {
-		elts := strings.SplitN(flag, "=", 2)
-		name := elts[0]
-		value := ""
-		if len(elts) == 2 {
-			value = elts[1]
-		}
-		switch name {
-		case "":
-			// no flags
-		case "-r":
-			*rewriteRule = value
-		case "-s":
-			*simplifyAST = true
-		case "-stdin":
-			// fake flag - pretend input is from stdin
-			info = nil
-		default:
-			t.Errorf("unrecognized flag name: %s", name)
-		}
-	}
+  // process flags
+  *simplifyAST = false
+  *rewriteRule = ""
+  info, err := os.Lstat(in)
+  if err != nil {
+    t.Error(err)
+    return
+  }
+  for _, flag := range strings.Split(gofmtFlags(in, 20), " ") {
+    elts := strings.SplitN(flag, "=", 2)
+    name := elts[0]
+    value := ""
+    if len(elts) == 2 {
+      value = elts[1]
+    }
+    switch name {
+    case "":
+      // no flags
+    case "-r":
+      *rewriteRule = value
+    case "-s":
+      *simplifyAST = true
+    case "-stdin":
+      // fake flag - pretend input is from stdin
+      info = nil
+    default:
+      t.Errorf("unrecognized flag name: %s", name)
+    }
+  }
 
-	initParserMode()
-	initRewrite()
+  initParserMode()
+  initRewrite()
 
-	const maxWeight = 2 << 20
-	var buf, errBuf bytes.Buffer
-	s := newSequencer(maxWeight, &buf, &errBuf)
-	s.Add(fileWeight(in, info), func(r *reporter) error {
-		return processFile(in, info, nil, r)
-	})
-	if errBuf.Len() > 0 {
-		t.Logf("%q", errBuf.Bytes())
-	}
-	if s.GetExitCode() != 0 {
-		t.Fail()
-	}
+  const maxWeight = 2 << 20
+  var buf, errBuf bytes.Buffer
+  s := newSequencer(maxWeight, &buf, &errBuf)
+  s.Add(fileWeight(in, info), func(r *reporter) error {
+    return processFile(in, info, nil, r)
+  })
+  if errBuf.Len() > 0 {
+    t.Logf("%q", errBuf.Bytes())
+  }
+  if s.GetExitCode() != 0 {
+    t.Fail()
+  }
 
-	expected, err := os.ReadFile(out)
-	if err != nil {
-		t.Error(err)
-		return
-	}
+  expected, err := os.ReadFile(out)
+  if err != nil {
+    t.Error(err)
+    return
+  }
 
-	if got := buf.Bytes(); !bytes.Equal(got, expected) {
-		if *update {
-			if in != out {
-				if err := os.WriteFile(out, got, 0666); err != nil {
-					t.Error(err)
-				}
-				return
-			}
-			// in == out: don't accidentally destroy input
-			t.Errorf("WARNING: -update did not rewrite input file %s", in)
-		}
+  if got := buf.Bytes(); !bytes.Equal(got, expected) {
+    if *update {
+      if in != out {
+        if err := os.WriteFile(out, got, 0666); err != nil {
+          t.Error(err)
+        }
+        return
+      }
+      // in == out: don't accidentally destroy input
+      t.Errorf("WARNING: -update did not rewrite input file %s", in)
+    }
 
-		t.Errorf("(gofmt %s) != %s (see %s.gofmt)\n%s", in, out, in,
-			diff.Diff("expected", expected, "got", got))
-		if err := os.WriteFile(in+".gofmt", got, 0666); err != nil {
-			t.Error(err)
-		}
-	}
+    t.Errorf("(gofmt %s) != %s (see %s.gofmt)\n%s", in, out, in,
+      diff.Diff("expected", expected, "got", got))
+    if err := os.WriteFile(in+".gofmt", got, 0666); err != nil {
+      t.Error(err)
+    }
+  }
 }
 
 // TestRewrite processes testdata/*.input files and compares them to the
@@ -135,62 +135,62 @@ func runTest(t *testing.T, in, out string) {
 //
 // in the processed file within the first 20 lines, if any.
 func TestRewrite(t *testing.T) {
-	// determine input files
-	match, err := filepath.Glob("testdata/*.input")
-	if err != nil {
-		t.Fatal(err)
-	}
+  // determine input files
+  match, err := filepath.Glob("testdata/*.input")
+  if err != nil {
+    t.Fatal(err)
+  }
 
-	// add larger examples
-	match = append(match, "gofmt.go", "gofmt_test.go")
+  // add larger examples
+  match = append(match, "gofmt.go", "gofmt_test.go")
 
-	for _, in := range match {
-		name := filepath.Base(in)
-		t.Run(name, func(t *testing.T) {
-			out := in // for files where input and output are identical
-			if strings.HasSuffix(in, ".input") {
-				out = in[:len(in)-len(".input")] + ".golden"
-			}
-			runTest(t, in, out)
-			if in != out && !t.Failed() {
-				// Check idempotence.
-				runTest(t, out, out)
-			}
-		})
-	}
+  for _, in := range match {
+    name := filepath.Base(in)
+    t.Run(name, func(t *testing.T) {
+      out := in // for files where input and output are identical
+      if strings.HasSuffix(in, ".input") {
+        out = in[:len(in)-len(".input")] + ".golden"
+      }
+      runTest(t, in, out)
+      if in != out && !t.Failed() {
+        // Check idempotence.
+        runTest(t, out, out)
+      }
+    })
+  }
 }
 
 // Test case for issue 3961.
 func TestCRLF(t *testing.T) {
-	const input = "testdata/crlf.input"   // must contain CR/LF's
-	const golden = "testdata/crlf.golden" // must not contain any CR's
+  const input = "testdata/crlf.input"   // must contain CR/LF's
+  const golden = "testdata/crlf.golden" // must not contain any CR's
 
-	data, err := os.ReadFile(input)
-	if err != nil {
-		t.Error(err)
-	}
-	if !bytes.Contains(data, []byte("\r\n")) {
-		t.Errorf("%s contains no CR/LF's", input)
-	}
+  data, err := os.ReadFile(input)
+  if err != nil {
+    t.Error(err)
+  }
+  if !bytes.Contains(data, []byte("\r\n")) {
+    t.Errorf("%s contains no CR/LF's", input)
+  }
 
-	data, err = os.ReadFile(golden)
-	if err != nil {
-		t.Error(err)
-	}
-	if bytes.Contains(data, []byte("\r")) {
-		t.Errorf("%s contains CR's", golden)
-	}
+  data, err = os.ReadFile(golden)
+  if err != nil {
+    t.Error(err)
+  }
+  if bytes.Contains(data, []byte("\r")) {
+    t.Errorf("%s contains CR's", golden)
+  }
 }
 
 func TestBackupFile(t *testing.T) {
-	dir, err := os.MkdirTemp("", "gofmt_test")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-	name, err := backupFile(filepath.Join(dir, "foo.go"), []byte("  package main"), 0644)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Created: %s", name)
+  dir, err := os.MkdirTemp("", "gofmt_test")
+  if err != nil {
+    t.Fatal(err)
+  }
+  defer os.RemoveAll(dir)
+  name, err := backupFile(filepath.Join(dir, "foo.go"), []byte("  package main"), 0644)
+  if err != nil {
+    t.Fatal(err)
+  }
+  t.Logf("Created: %s", name)
 }
